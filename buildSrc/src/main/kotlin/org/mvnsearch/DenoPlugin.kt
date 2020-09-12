e01a54f299bcf3f10a6335275c735e6aca86cd3d
linux_china <libing.chen@gmail.com>
2020-09-12 02:30:40 -0700

    deno plugin

---
+++ buildSrc/src/main/kotlin/org/mvnsearch/DenoPlugin.kt
@@ -0,0 +1,10 @@
+package org.mvnsearch
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+open class DenoPlugin : Plugin<Project> {
+  override fun apply(project: Project) {
+    // println("Project: " + project.name)
+  }
+}

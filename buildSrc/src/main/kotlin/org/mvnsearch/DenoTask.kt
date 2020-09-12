9eb7ea7e810f9499312276e8a117ed0b675e3e23
linux_china <libing.chen@gmail.com>
2020-09-12 02:31:17 -0700

    DenoTask.kt

---
+++ buildSrc/src/main/kotlin/org/mvnsearch/DenoTask.kt
@@ -0,0 +1,25 @@
+package org.mvnsearch
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+import java.io.File
+
+open class DenoTask : DefaultTask() {
+  @TaskAction
+  fun execute() {
+    val appName = project.name
+    val scriptDir = File(project.projectDir, "buildSrc/src/scripts")
+    val templateCode = File(scriptDir, "template.js").readText()
+    val depsJsCode = File(scriptDir, "deps.js").readText()
+    val compiledJsCode = File(project.buildDir, "js/packages/${appName}/kotlin/${appName}.js").readText()
+    var denoCode = templateCode.replace("//deps.js", depsJsCode)
+    denoCode = templateCode.replace("//compiledCode", compiledJsCode)
+    val destDir = File(project.buildDir, "deno")
+    if (!destDir.exists()) {
+      destDir.mkdirs()
+    }
+    val denoFile = File(destDir, "${appName}.js")
+    denoFile.writeText(denoCode)
+    println("Deno file generated: $denoFile")
+  }
+}

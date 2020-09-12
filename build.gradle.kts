96f8cdd34beeeca4bd5abd9be6499fcd5dce26d8
linux_china <libing.chen@gmail.com>
2020-09-12 02:32:30 -0700

    add deno task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
   kotlin("js") version "1.4.10"
+  id("deno-plugin")
 }
+
 group = "org.mvnsearch"
 version = "1.0.0-SNAPSHOT"
 
@@ -17,5 +19,14 @@ kotlin {
     useCommonJs()
     binaries.executable()
     nodejs()
+  }
+}
+
+tasks {
+  //task from buildSrc
+  register<org.mvnsearch.DenoTask>("deno") {
+    dependsOn("build")
+    group = "Deno"
+    description = "Build Deno file from Kotlin"
   }
 }

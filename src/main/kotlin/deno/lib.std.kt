d056bd9bfa887e8704d2c1cfafc6334bd13e698f
linux_china <libing.chen@gmail.com>
2020-09-12 19:23:21 -0700

    add declarations

--- src/main/kotlin/deno/lib.std.kt
+++ src/main/kotlin/deno/lib.std.kt
@@ -2,3 +2,13 @@
 
 external fun encode(input: String): String
 external fun decode(input: String): String
+
+external fun createHash(algorithm: String): Hasher;
+
+external interface Hasher {
+  fun update(data: String): Hasher;
+  fun update(data: ArrayBuffer): Hasher;
+  fun digest(): ArrayBuffer;
+  override fun toString(): String;
+  fun toString(format: String): String;
+}

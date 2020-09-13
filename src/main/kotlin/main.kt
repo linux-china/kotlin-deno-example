3e894aa3bae4f733a2956e087a4da6fd91f77122
linux_china <libing.chen@gmail.com>
2020-09-13 00:13:46 -0700

    adjust main method to suspend

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,16 +1,16 @@
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import user.UserManager
 
-fun main() {
+suspend fun main() = coroutineScope {
   val names = sequenceOf("first", "second", "third")
   names.map { it.length }
     .forEach { println(it) }
   val userManager = UserManager()
   println(userManager.findNickById(1))
   println(Deno.version.deno)
-  GlobalScope.launch { // launch a new coroutine in background and continue
+  launch { // launch a new coroutine in background and continue
     delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
     println("Hello World!") // print after delay
   }

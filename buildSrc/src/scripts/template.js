7c59c9fd8fe0fb6167095ac82b7a426269911314
linux_china <libing.chen@gmail.com>
2020-09-12 18:21:30 -0700

    add kotlin-coroutines-core

--- buildSrc/src/scripts/template.js
+++ buildSrc/src/scripts/template.js
@@ -1,19 +1,22 @@
-import Kotlin from "https://jspm.dev/kotlin@1.4"
+import Kotlin from "https://jspm.dev/kotlin@1.4.0"
+import KotlinxCoroutinesCore from "https://jspm.dev/kotlinx-coroutines-core@1.3.9"
 
 //===== global imports ===========
 //deps.js
 
 //===== stub for commonjs ===========
 const module = {
-    exports: {}
+  exports: {}
 };
 
 function require(moduleName) {
-    if (moduleName === 'kotlin') {
-        return Kotlin
-    } else {
-        return {};
-    }
+  if (moduleName === 'kotlin') {
+    return Kotlin
+  } else if (moduleName === 'kotlinx-coroutines-core') {
+    return KotlinxCoroutinesCore;
+  } else {
+    return {};
+  }
 }
 
 //===== compiled Javascript code from Kotlin ====

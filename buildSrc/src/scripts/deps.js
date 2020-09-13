1532d5676bc7732298dbf4a75446634028f83009
linux_china <libing.chen@gmail.com>
2020-09-12 19:22:36 -0700

    disable imports

--- buildSrc/src/scripts/deps.js
+++ buildSrc/src/scripts/deps.js
@@ -1,2 +1,4 @@
 // noinspection ES6UnusedImports
-import {encode, decode} from "https://deno.land/std@0.68.0/encoding/utf8.ts"
+// import {encode, decode} from "https://deno.land/std@0.68.0/encoding/utf8.ts"
+// noinspection ES6UnusedImports
+// import {createHash} from "https://deno.land/std@0.68.0/hash/mod.ts";
